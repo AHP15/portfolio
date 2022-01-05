cafc9e089de73acda4b006614f72fdeed8050a7c
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-01-05 11:01:01 +0100

    remove files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Home from './components/Home';
-import MyForm from './endEmail';
 
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Home />
-      <MyForm />
     </div>
   );
 }
